875a72f6682b26d09ad1a9e3382d1236393f86e3
Daniel <dlei@qcti.net>
2022-06-05 21:57:16 -0400

    commit on 'main'

---
+++ GamePanel.java
@@ -0,0 +1,35 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+    // Screen settings
+    final int originalTileSize = 16; // 16 x 16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48 x 48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
+
+    public GamePanel () {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run(){
+
+    }
+
+
+}
