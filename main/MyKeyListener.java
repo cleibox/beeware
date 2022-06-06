e615d2f7f3e555883a966272e6da812e4d390031
phoebe17178 <phoebe.y375@gmail.com>
2022-06-06 15:53:06 -0400

    Create MyKeyListener.java

---
+++ main/MyKeyListener.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyKeyListener implements KeyListener {
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // not used
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();// returns integer associated with each key
+
+        if (code == KeyEvent.VK_UP) { // if 'up arrow' is pressed
+        upPressed = true;
+        }
+
+        if (code == KeyEvent.VK_DOWN) { // if 'down arrow' is pressed
+        downPressed = true;
+        }
+
+        if (code == KeyEvent.VK_LEFT) { // if 'left arrow' is pressed
+        leftPressed = true;
+        }
+
+        if (code == KeyEvent.VK_RIGHT) { // if 'right arrow' is pressed
+        rightPressed = true;
+        }
+
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();// returns integer associated with each key
+       
+        if (code == KeyEvent.VK_UP) { // if 'up arrow' is pressed
+        upPressed = false;
+        }
+
+        if (code == KeyEvent.VK_DOWN) { // if 'down arrow' is pressed
+        downPressed = false;
+        }
+
+        if (code == KeyEvent.VK_LEFT) { // if 'left arrow' is pressed
+        leftPressed = false;
+        }
+
+        if (code == KeyEvent.VK_RIGHT) { // if 'right arrow' is pressed
+        rightPressed = false;
+        }
+    }
+}
