b4889e0baae233cce238bd16509bc34d9cb3f042
phoebe17178 <phoebe.y375@gmail.com>
2022-06-06 14:06:35 -0400

    Create Main.java

---
+++ main/Main.java
@@ -0,0 +1,25 @@
+/* BeewareOfTheGarden
+ * Desc: INSERT
+ * @Cynthia L & Phoebe Y ICS4U
+ * @version Jun 2022
+ */
+
+ package main;
+ import javax.swing.JFrame;
+
+ public class Main{
+     public static void main (String [] args){
+         
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows the window to close properly
+        window.setResizable (false);
+        window.setTitle ("BEEware Of the Garden");
+        
+        window.setLocationRelativeTo(null); //window will be displyed at the center of the screen 
+        window.setVisible(true);
+
+        //4:08 video 1 #ryisnowslay
+     }
+    }
+
+
