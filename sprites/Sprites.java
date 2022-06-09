f3d2e99cd6955be9755df8bbe8a035116ff45695
phoebe17178 <phoebe.y375@gmail.com>
2022-06-09 15:06:46 -0400

    Update Sprites.java

--- sprites/Sprites.java
+++ sprites/Sprites.java
@@ -1,6 +1,16 @@
 package sprites;
 
+import java.awt.image.BufferedImage;
+
 public class Sprites {
     public int x, y;
-    public int speed;  
+    public int speed;
+    
+    public BufferedImage beeLeft, beeRight;
+    public BufferedImage playerUp, playerUp1, playerUp2;
+    public BufferedImage playerDown, playerDown1, playerDown2;
+    public BufferedImage playerLeft, playerLeft1;
+    public BufferedImage playerRight, playerRight1;
+
+    public String direction;
 }
